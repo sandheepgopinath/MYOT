8a167613ca837750a5015e7093ac8d37cacb5383
Sandheep Gopinath <sandheepgopinath@gmail.com>
2026-01-26 14:59:30 +0000

    Make all buttons and header glass morphic as in apple ios. The color of

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import { ThemeSwitcher } from "./theme-switcher";
 export default function Header() {
   return (
     <header className="py-4 px-4 sm:px-6 lg:px-8 absolute top-0 w-full z-20">
-      <div className="container mx-auto flex items-center justify-between text-white glass-card p-4">
+      <div className="container mx-auto flex items-center justify-between text-text-primary glass-card p-4">
         <Link href="/">
           <h1 className="text-2xl font-brand tracking-widest uppercase">
             MAKE MY TEE.
@@ -13,7 +13,7 @@ export default function Header() {
         <nav className="flex items-center gap-2 md:gap-6">
           <Link
             href="#"
-            className="text-sm uppercase tracking-wider text-text-secondary hover:text-text-primary transition-colors nav-link-desktop"
+            className="btn-outline uppercase nav-link-desktop !py-2 !px-4 text-xs"
           >
             How it works
           </Link>
