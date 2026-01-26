0d6253f47040d1f25ba8d86b36c65be7315443e9
Sandheep Gopinath <sandheepgopinath@gmail.com>
2026-01-26 15:57:34 +0000

    Make the text normal and not bld. Keep it small (_for element a_)

--- src/components/myot/hero.tsx
+++ src/components/myot/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <h2 className="font-tagline text-2xl md:text-3xl premium-gradient mb-4">
           Designed by you, crafted by us.
         </h2>
-        <a href="#" className="btn-outline uppercase">
+        <a href="#" className="btn-outline font-normal text-sm">
           Start Crafting
         </a>
       </div>
