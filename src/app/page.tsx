f45abc6e42e28b999c886fa72931a0c74be04ba0
Sandheep Gopinath <sandheepgopinath@gmail.com>
2026-01-30 19:46:29 +0000

    Page doesn't scroll beyond personalize in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     <div className="flex flex-col">
       <Header scrollY={scrollY} />
       {/* This outer div creates the scrollable height */}
-      <div className="relative h-[1300vh] w-full">
+      <div className="relative h-[2500vh] w-full">
         {/* This main tag will stick to the top */}
         <main className="sticky top-0 h-screen w-full overflow-hidden">
           <Hero scrollY={scrollY} />
